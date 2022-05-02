817c978392f18b6618653952e6bbb96a3768d233
Pacodiz02 <73858527+Pacodiz02@users.noreply.github.com>
2022-05-02 12:38:41 +0200

    probado mysql

--- MYSQL/Esquema-MySQL.sql
+++ MYSQL/Esquema-MySQL.sql
@@ -2,14 +2,13 @@
 
 CREATE TABLE SOCIOS (
 DNI VARCHAR(9),
-Nombre VARCHAR(20),
+Nombre VARCHAR(20) NOT NULL,
 Direccion VARCHAR(20),
 Telefono VARCHAR(9),
 email VARCHAR(20),
 CONSTRAINT pk_socios PRIMARY KEY (DNI),
-CONSTRAINT nn_Nombre CHECK (Nombre IS NOT NULL),
 CONSTRAINT ck_dni_socios CHECK (DNI REGEXP '^[0-9]{8}[A-Z]{1}'),
-CONSTRAINT ck_telefono CHECK (Telefono REGEXP '^[0-9]{9}'))
+CONSTRAINT ck_telefono CHECK (Telefono REGEXP '^[0-9]{9}')
 );
 
 CREATE TABLE PENALIZACIONES (
@@ -24,12 +23,11 @@ CONSTRAINT ck_fechapenalizaciones CHECK (MONTH(Fecha_penalizacion) NOT BETWEEN 0
 
 CREATE TABLE LIBROS (
 ISBN VARCHAR(13),
-Titulo VARCHAR(30),
+Titulo VARCHAR(30) NOT NULL,
 Genero VARCHAR(10),
 AnioPublicacion INT(4),
 Editorial VARCHAR(10),
 CONSTRAINT pk_libros PRIMARY KEY (ISBN),
-CONSTRAINT nn_Titulo CHECK(Titulo IS NOT NULL),
 CONSTRAINT ck_mayusculas_editorial CHECK (UPPER(Editorial) = Editorial)
 );
 
@@ -44,10 +42,9 @@ CONSTRAINT fk_ejemplares_libros FOREIGN KEY (ISBNLibroE) REFERENCES LIBROS(ISBN)
 
 CREATE TABLE AUTOR ( 
 Nombre_autor VARCHAR(40),
-FechaNac DATE,
+FechaNac DATE NOT NULL,
 Nacionalidad VARCHAR(15),
 CONSTRAINT pk_autor PRIMARY KEY (Nombre_autor),
-CONSTRAINT nn_FechaNac CHECK (FechaNac IS NOT NULL),
 CONSTRAINT ck_FechaNac CHECK (YEAR(FechaNac) > 1900)
 );
 
@@ -70,19 +67,18 @@ CONSTRAINT pk_empleados PRIMARY KEY (DNI),
 CONSTRAINT ck_Nombre_empleado CHECK (Nombre REGEXP '^[A-Z]\.*[A-Z]\.'),
 CONSTRAINT uk_empleados UNIQUE (Direccion),
 CONSTRAINT ck_genero_empleado CHECK (Genero IN ('M', 'F')),
-CONSTRAINT ck_dni_empleados CHECK (DNI REGEXP '^[0-9]{8}[A-Z]{1}'))
+CONSTRAINT ck_dni_empleados CHECK (DNI REGEXP '^[0-9]{8}[A-Z]{1}')
 );
 
 
 CREATE TABLE PROOVEDORES (
 CIF VARCHAR(9),
-Nombre VARCHAR(30),
+Nombre VARCHAR(30) NOT NULL,
 Telefono VARCHAR(9),
 Email VARCHAR(20),
 CONSTRAINT pk_proovedores PRIMARY KEY (CIF),
-CONSTRAINT nn_Nombre_proovedor CHECK (Nombre IS NOT NULL),
 CONSTRAINT uk_proovedores UNIQUE (Telefono),
-CONSTRAINT ck_telefono_proovedor CHECK (Telefono REGEXP '^[9]{1}[0-9]{8}'))
+CONSTRAINT ck_telefono_proovedor CHECK (Telefono REGEXP '^[9]{1}[0-9]{8}')
 );
 
 
@@ -91,12 +87,11 @@ FechaPrestamo DATE,
 DNI_Socio VARCHAR(9),
 Cod_ejemplarP VARCHAR(9),
 DNI_empP VARCHAR(9),
-FechaDevolucion DATE,
+FechaDevolucion DATE NOT NULL,
 CONSTRAINT pk_prestamos PRIMARY KEY (FechaPrestamo, DNI_Socio, Cod_ejemplarP),
 CONSTRAINT fk_prestamos_socios FOREIGN KEY (DNI_Socio) REFERENCES SOCIOS(DNI),
 CONSTRAINT fk_prestamos_ejemplares FOREIGN KEY (Cod_ejemplarP) REFERENCES EJEMPLARES(Cod_ejemplar),
 CONSTRAINT fk_prestamos_empleados FOREIGN KEY (DNI_empP) REFERENCES EMPLEADOS(DNI),
-CONSTRAINT nn_FechaDevolucion CHECK (FechaDevolucion IS NOT NULL),
 CONSTRAINT ck_fechadevolucion CHECK (FechaDevolucion > FechaPrestamo)
 );
 
@@ -105,11 +100,10 @@ CREATE TABLE PROOV (
 Fecha_proov DATE,
 CIF_Pr VARCHAR(9),
 ISBN_LibroPr VARCHAR(13),
-Cantidad NUMBER(9) DEFAULT 1,
+Cantidad INT(9) DEFAULT 1 NOT NULL,
 CONSTRAINT pk_proov PRIMARY KEY (Fecha_proov, CIF_Pr, ISBN_LibroPr),
 CONSTRAINT fk_proov_proovedores FOREIGN KEY (CIF_Pr) REFERENCES PROOVEDORES(CIF),
-CONSTRAINT fk_proov_libros FOREIGN KEY (ISBN_LibroPr) REFERENCES LIBROS(ISBN),
-CONSTRAINT nn_Cantidad CHECK (Cantidad IS NOT NULL)
+CONSTRAINT fk_proov_libros FOREIGN KEY (ISBN_LibroPr) REFERENCES LIBROS(ISBN)
 );
 
 
@@ -129,7 +123,7 @@ ALTER TABLE SOCIOS ADD Fecha_nacimiento DATE;
 ALTER TABLE EMPLEADOS ADD tipo VARCHAR(15);
 
 -- Modifica la columna tipo en la tabla EMPLEADOS y pon por defecto 'TEMPORAL'.
-ALTER TABLE EMPLEADOS MODIFY tipo DEFAULT 'TEMPORAL';
+ALTER TABLE EMPLEADOS MODIFY tipo VARCHAR(15) DEFAULT 'TEMPORAL';
 
 -- Modifica la columna AñoPublicacion de la tabla LIBROS cambiando el tipo de dato a fecha.
 ALTER TABLE LIBROS MODIFY AnioPublicacion DATE;
@@ -138,13 +132,13 @@ ALTER TABLE LIBROS MODIFY AnioPublicacion DATE;
 ALTER TABLE PENALIZACIONES MODIFY Observacion VARCHAR(500);
 
 -- Elimina la columna email de la tabla SOCIOS.
-ALTER TABLE SOCIOS DROP COLUMN email VARCHAR(20);
+ALTER TABLE SOCIOS DROP COLUMN email;
 
 -- La Nacionalidad de los autores estará comprendida entre las siguientes: Española, Francesa, Italiana, Americana, Alemana y Japonesa.
 ALTER TABLE AUTOR ADD CONSTRAINT ck_nacionalidad_autores CHECK (Nacionalidad IN ('Española', 'Francesa', 'Italiana', 'Americana', 'Alemana', 'Japonesa'));
 
 -- La referencia de los libros solo tienen carácteres numéricos, aunque sigue siendo de tipo cadena.
-ALTER TABLE LIBROS ADD CONSTRAINT ck_ISBN_libros CHECK (ISBN REGEXP '^[0-9]{13}$'));
+ALTER TABLE LIBROS ADD CONSTRAINT ck_ISBN_libros CHECK (ISBN REGEXP '^[0-9]{13}$');
 
 -- Elimina la restricción de la columna Dirección de la tabla EMPLEADOS.
 ALTER TABLE EMPLEADOS DROP CONSTRAINT uk_empleados;
